bc4bcb373e7bb52258066bbb6968ac0e4769af9d
programmer55C <cmb@christianbrand.de>
2018-03-27 16:23:11 +0200

    Added entry point to geogebra render

---
+++ plugins/content/geogebra/src/index.render.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Display from './Component/Display'
+
+export default {
+  Component: Display,
+  name: 'serlo/content/geogebra',
+  version: '0.0.1',
+  text: 'Geogebra'
+}
